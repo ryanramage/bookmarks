7f8959e590deab4f5753e2ea6208c15ff567d420
Ryan Ramage <ryan.ramage@gmail.com>
2012-04-21 11:06:58 -0600

    Get all the three tabs (recent,review,views) working.

--- static/js/ui.js
+++ static/js/ui.js
@@ -15,11 +15,13 @@ $(function() {
 
 
     $('.row.topic').live('mouseenter', function(){
-        console.log('in');
         $(this).find('.actions').show();
     }).live('mouseleave', function() {
         $(this).find('.actions').hide();
     });
+
+
+
 
 
     $('.archive').live('click', function(){
@@ -61,15 +63,64 @@ $(function() {
         return false;
     })
 
-    db.getView('bookmarks', 'by_date', {include_docs:true, descending: true}, function(err, data) {
-        if (err) return humane.error(err);
+    function render_data(data) {
+        $('.bookmarks').empty();
         _.each(data.rows, function(row) {
             var random = Math.floor(Math.random()*10000);
             row.doc.random = random;
             $('.bookmarks').append(handlebars.templates['bookmark_row.html'](row.doc, {}));
-        })
+            $('img.site-image').error(function() {
+                 $(this).attr('src', 'static/img/ark2.png');
+            });
+        });
+    }
 
-    })
+    function activate_tab(tab) {
+        $('ul.topicFilter li').removeClass('active');
+        $('ul.topicFilter li.' + tab).addClass('active');
+    }
+
+
+    function recent() {
+        if (checkLastRoute('recent')) return;
+        activate_tab('recent');
+        db.getView('bookmarks', 'by_date', {include_docs:true, descending: true, random : Math.floor(Math.random()*10000) }, function(err, data) {
+            if (err) return humane.error(err);
+            render_data(data);
+        });
+    }
+    function to_review() {
+        if (checkLastRoute('to-review')) return;
+        activate_tab('to-review');
+        db.getView('bookmarks', 'by_views', {include_docs:true, descending: true, endkey : [0], startkey: [0, {}], random : Math.floor(Math.random()*10000) }, function(err, data) {
+            if (err) return humane.error(err);
+            render_data(data);
+        });
+    }
+
+    function views() {
+        if (checkLastRoute('view')) return;
+        activate_tab('view');
+        db.getView('bookmarks', 'by_views', {include_docs:true, descending: true, endkey : [1, {}], startkey: [Number.MAX_VALUE, {}], random : Math.floor(Math.random()*10000)  }, function(err, data) {
+            if (err) return humane.error(err);
+            render_data(data);
+        });
+    }
+
+    var last_route = null;
+    function checkLastRoute(route) {
+        if (last_route == route) return true;
+        last_route = route;
+        return false;
+    }
+
+    var routes = {
+      '/recent' : recent,
+      '/to_review' : to_review,
+      '/views'   : views
+    };
+    var router = Router(routes);
+    router.init('/recent');
 
 
 
