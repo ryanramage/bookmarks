11326bb9bf0fe0773fcbec8d43fcab4bb53606c7
Ryan Ramage <ryan.ramage@gmail.com>
2012-04-26 22:19:45 -0600

    Add ability to add tags during bookmarking

--- static/js/newBookmarkFull.js
+++ static/js/newBookmarkFull.js
@@ -5,7 +5,40 @@ $(function(){
     }
 
 
-    $('#topicTags').chosen({no_results_text: "No results matched"});
+    var createHash = function(text) {
+       return text.toLowerCase().replace(/ /g, '_');
+    }
+
+
+    $('#topicTags').chosen({no_results_text: "No results matched", create_option : function(terms){
+
+        var me = this;
+
+        var hash = terms.toLowerCase().replace(/ /g, '_');
+
+        var tag = {
+            type : 'garden.tag',
+            name : terms,
+            hash : hash
+        };
+        $.ajax({
+              url :  './_db' ,
+              type: 'POST',
+              contentType : 'application/json',
+              data : JSON.stringify(tag),
+              success : function(result) {
+                  return me.select_append_option({
+                    value: terms,
+                    text: terms
+                  });
+              },
+              error : function() {
+                  console.log('error');
+              }
+        });
+
+
+    }});
 
 
     $('form').on('submit', function() {
